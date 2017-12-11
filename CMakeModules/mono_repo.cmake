119db87c173cd5fcb677128306e4e2ca853b6943
David Allemant <david.allemant@gmail.com>
2017-12-11 20:55:50 +0100

    reorder functions, add commentaries

--- CMakeModules/mono_repo.cmake
+++ CMakeModules/mono_repo.cmake
@@ -1,31 +1,18 @@
+######### load_all_components  ############################
+# find all components declared in source tree
+# add only selected(and their dependencies) in CMakeCache.txt
 
-
-include(Map)
-
-######### IMPORTED_COMPONENTS Global LIST ########
-define_property(GLOBAL PROPERTY IMPORTED_COMPONENTS
-    BRIEF_DOCS "Global list of components"
-    FULL_DOCS "Global list of components")
-# Initialize property
-set_property(GLOBAL PROPERTY IMPORTED_COMPONENTS "")
-
-############### private macro register_component ################
-macro(register_component var)
-
-    set_property(GLOBAL APPEND PROPERTY IMPORTED_COMPONENTS "${var}")
-endmacro(register_component)
-
-###################################################
-function(print_message_if_no_component_selected)
-    get_property(my_list_content GLOBAL PROPERTY IMPORTED_COMPONENTS)
-
-    if(NOT my_list_content)
-        message(FATAL_ERROR " no component selected; select any in CMakeCache.txt    ")
-    endif()
-endfunction()
+# call it in CMakeLists.txt root source dir
+###########################################################
+macro(load_all_components)
+    file( GLOB_RECURSE project_files    component.cmake )
+    _load_component_list("${project_files}")
+endmacro()
 
 ######### import  ########################################
 # replacement of find_package for monorepo build system
+# use it in your CMakeLists.txt to declare dependency to another
+# component in the build tree
 ###########################################################
 macro(import component)
 
@@ -35,7 +22,7 @@ macro(import component)
         #already imported
     else()
         if(EXISTS ${CMAKE_SOURCE_DIR}/${component})
-            register_component(${component} )
+            _register_component(${component} )
             #import
             add_subdirectory( ${CMAKE_SOURCE_DIR}/${component} ${CMAKE_BINARY_DIR}/${component} )
         else()
@@ -44,6 +31,11 @@ macro(import component)
     endif()
 endmacro()
 
+############### declare_component ###########################
+# for each component, create a file named component.cmake
+# add call this macro with the name of the target as
+# argument
+#############################################################
 macro(declare_component component)
 
     message("declare component located: ${CMAKE_CURRENT_LIST_DIR}")
@@ -55,30 +47,42 @@ macro(declare_component component)
 
 endmacro()
 
+#______________________________________________________________________
+#                       private implementation
+#______________________________________________________________________
+
+include(Map)
+
+######### IMPORTED_COMPONENTS Global LIST ########
+define_property(GLOBAL PROPERTY IMPORTED_COMPONENTS
+    BRIEF_DOCS "Global list of components"
+    FULL_DOCS "Global list of components")
+# Initialize property
+set_property(GLOBAL PROPERTY IMPORTED_COMPONENTS "")
+
+############### private macro _register_component ################
+macro(_register_component var)
+
+    set_property(GLOBAL APPEND PROPERTY IMPORTED_COMPONENTS "${var}")
+endmacro(_register_component)
 
 
-##########################################################################
-##### find all component.cmake in first level subdirectories##############
-file( GLOB project_files
-        ${CMAKE_CURRENT_SOURCE_DIR}/*/component.cmake
-)
 
-#########################################################
-##### find all component.cmake in all tree ##############
-#file( GLOB_RECURSE project_files    component.cmake )
+###################################################
+function(_print_message_if_no_component_selected)
+    get_property(my_list_content GLOBAL PROPERTY IMPORTED_COMPONENTS)
 
-message("/////////////////////\n ${project_files} \n//////////////////////\n")
+    if(NOT my_list_content)
+        message(FATAL_ERROR " no component selected; select any in CMakeCache.txt    ")
+    endif()
+endfunction()
 
-foreach( project_file ${project_files} )
+
+macro(_load_component_list components)
+    message("****** ${components}")
+    foreach( project_file ${components} )
         include( ${project_file} )
-endforeach()
-print_message_if_no_component_selected()
+    endforeach()
+    _print_message_if_no_component_selected()
+endmacro()
 
-
-MACRO(INSERT_INTO_MAP _KEY _VALUE)
-  SET("MyMap_${_KEY}" "${_VALUE}")
-ENDMACRO(INSERT_INTO_MAP)
-
-SET(MyKey "foo")
-SET(MyValue "bar")
-#INSERT_INTO_MAP("${MyKey}" "${MyValue}")
